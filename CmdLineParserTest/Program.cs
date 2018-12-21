757468f9026b0d4686e326c1a136a77994742bd4
John Lundgren <john.paul.lundgren@gmail.com>
2018-12-21 11:37:20 +0100

    Using only two options

--- CmdLineParserTest/Program.cs
+++ CmdLineParserTest/Program.cs
@@ -7,30 +7,23 @@ namespace CmdLineParserTest
 
     class Options
     {
+        [Option('v', "verbose", Required = true, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+
         [Option('r', "read", Required = true, HelpText = "Input files to be processed.")]
         public IEnumerable<string> InputFiles { get; set; }
 
-        // Omitting long name, defaults to name of property, ie "--verbose"
-        [Option(
-          Default = false,
-          HelpText = "Prints all messages to standard output.")]
-        public bool Verbose { get; set; }
+        //[Option("stdin",
+        //  Default = false, 
+        //    HelpText = "Read from stdin")]
+        //public bool stdin { get; set; }
 
-        [Option("stdin",
-          Default = false, 
-            HelpText = "Read from stdin")]
-        public bool stdin { get; set; }
-
-        [Value(0, MetaName = "offset", HelpText = "File offset.")]
-        public long? Offset { get; set; }
+        //[Value(0, MetaName = "offset", HelpText = "File offset.")]
+        //public long? Offset { get; set; }
     }
 
     class Program
     {
-
-        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
-        public bool Verbose { get; set; }
-
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
